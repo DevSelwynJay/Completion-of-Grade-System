4a5be1fb8a6b8d5e904b9b22169574387c23dcb8
Jay <selwynjayf@gmail.com>
2022-04-08 16:56:38 +0800

    sass and custom bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './custom_bootstrap/custom_bootstrap.scss';
-import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-
-      <section className='py-5'>
-        <div className="container">
-          <h1>Need a Wireframe</h1>
-          <Button>Click</Button>
-        </div>
-      </section>
+       
+       <section className='py-3'>
+         <div className='container'>
+            <h1 className='text-center'>Hello</h1>
+            <button className='btn btn-primary'>Click</button>
+         </div>
+       </section>
 
     </div>
   );
