3e20207c64fd0ab37b0eac20898bec062465f4ad
Jay <selwynjayf@gmail.com>
2022-04-08 16:09:06 +0800

    added scss package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './custom_bootstrap/custom_bootstrap.scss';
 import { Button } from 'react-bootstrap';
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
       <section className='py-5'>
         <div className="container">
           <h1>Need a Wireframe</h1>
+          <Button>Click</Button>
         </div>
       </section>
 
