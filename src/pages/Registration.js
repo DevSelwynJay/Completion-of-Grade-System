4d1a61a99acb2426bc04d000b4b49418d3444155
Jay <selwynjayf@gmail.com>
2022-04-14 11:58:05 +0800

    adding icons

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -2,7 +2,10 @@ import '../pages/Registration.scss';
 import Logo from '../images/bulsu-logo.png';
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 
 function Registration(){
@@ -43,21 +46,41 @@ function Registration(){
                     <h1 className='text-center mb-3 fw-bold'>Registration</h1>
                         <div className='row'>
                            <div className='col-sm-6 mb-3'>
-                              <input type="text" className="form-control " placeholder="First name"/>
+                              <div className='control has-icon-left'>
+                                <span className='is-left'>
+                                   <FontAwesomeIcon icon={faUser} />
+                                </span>
+                                <input type="text" className="form-control " placeholder="First name"/>
+                              </div>
                            </div>
                            <div className='col-sm-6 mb-3'>
+                              <div className='control has-icon-left'>
+                              <span className='is-left'>
+                                 <FontAwesomeIcon icon={faUser} />
+                              </span>
                               <input type="text" className="form-control " placeholder="Last name"/> 
+                              </div>
                            </div>
                         </div>
                         <div className='row'>
                            <div className='col-sm-6 mb-3'>
-                              <input type="text" className="form-control " placeholder="Contact No."/>
+                              <div className='control has-icon-left'>
+                                <span className='is-left'>
+                                   <FontAwesomeIcon icon={faPhone} />
+                                </span>
+                                <input type="text" className="form-control " placeholder="Contact No."/>
+                              </div>
                            </div>
                            <div className='col-sm-6 mb-3'>
-                            <select value={value} onChange={handleChange} className="form-select" aria-label="Default select example">
-                            <option value="Faculty" >Faculty</option>
-                            <option value="Student">Student</option>
-                            </select>
+                             <div className='control has-icon-left'>
+                                 <span className='is-left'>
+                                    <FontAwesomeIcon icon={faUsers} />
+                                 </span>
+                                 <select value={value} onChange={handleChange} className="form-select" aria-label="Default select example">
+                                 <option value="Faculty" >Faculty</option>
+                                 <option value="Student">Student</option>
+                                 </select>
+                             </div>
                            </div>
                         </div>
                         {value==="Student" && ( // If value is equal to student, these div will mount.
